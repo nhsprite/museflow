b3b0bdb3cb260457b528e7ed4f739bdee63e508c
nihao.sensation <nihao.sensation@gmail.com>
2026-04-25 13:03:01 +0800

    [rewrite] remove debug log

--- src/cli/commands/rewrite.ts
+++ src/cli/commands/rewrite.ts
@@ -233,7 +233,6 @@ async function rewriteChapter(storyId: string, userResponse: boolean, targetChap
       writeOneChapterOnly: true,
     }
 
-    console.log(`[DEBUG] Deleting chapters ${targetChapterIndex + 1} to ${checkpointState.totalChapters}`)
     for (let ch = targetChapterIndex + 1; ch <= checkpointState.totalChapters; ch++) {
       await deleteChapterContent(outputDir, ch)
     }
